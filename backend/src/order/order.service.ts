a2d15b265ff179253e7bcec23c5a426ec0f30350
dima <komiksmr99@gmail.com>
2026-03-23 21:33:55 +0300

    0.3

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateOrderDTO } from './dto/order.dto';
 import { FilmRepository } from '../repository/film.repository';
 
@@ -7,17 +11,19 @@ export class OrderService {
   constructor(private filmRepository: FilmRepository) {}
 
   async createOrder(tickets: CreateOrderDTO[]) {
+    const seatsSet = new Set();
+
     for (const ticket of tickets) {
       const film = await this.filmRepository.findById(ticket.film);
 
       if (!film) {
-        throw new Error(`Фильм ${ticket.film} не найден`);
+        throw new NotFoundException(`Фильм ${ticket.film} не найден`);
       }
 
       const session = film.schedule.find((s) => s.id === ticket.session);
 
       if (!session) {
-        throw new Error(`Сеанс ${ticket.session} не найден`);
+        throw new NotFoundException(`Сеанс ${ticket.session} не найден`);
       }
 
       session.taken = session.taken || [];
@@ -27,6 +33,11 @@ export class OrderService {
         throw new Error(`Место ${newSeat} занято`);
       }
 
+      if (seatsSet.has(newSeat)) {
+        throw new BadRequestException('Duplicate seats in request');
+      }
+
+      seatsSet.add(newSeat);
       session.taken.push(newSeat);
 
       film.markModified('schedule');
