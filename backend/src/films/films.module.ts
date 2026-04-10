39e182c6c5f2bec808a0588325a1e2db714bb226
dima <komiksmr99@gmail.com>
2026-04-10 23:48:40 +0300

    pr16-2

--- backend/src/films/films.module.ts
+++ backend/src/films/films.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { FilmsController } from './films.controller';
 import { FilmsService } from './films.service';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { Film, FilmSchema } from './schema/films.schema';
 import { FilmRepository } from '../repository/film.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Film } from './entity/film.entity';
